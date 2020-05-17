2bdedaab70e41700df0fb05494fe55c3fc8a08e9
Mahdi <maouc001@ucr.edu>
2020-05-17 16:50:31 -0700

    Completed part 3

--- source/main.c
+++ source/main.c
@@ -1,14 +1,13 @@
 /*	Author: Mahdi Aouchiche
  *      Partner(s) Name: 
  *	Lab Section:
- *	Assignment: Lab # 8  Exercise # 2
+ *	Assignment: Lab # 8  Exercise # 3
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */ 
-
-// Demo video: https://drive.google.com/open?id=12VCaCqwC5DnB08wLmEHdA3MADQTl8lYd
+// Demo video: https://drive.google.com/open?id=1IOQzq_eq-TbExVm_OCJ677v1iDt-2f3x
  
 #include <avr/io.h>
 #include "timer.h"
@@ -16,10 +15,36 @@
 #include "simAVRHeader.h"
 #endif
 
-unsigned short x ;      // x holds the value of ADC
-unsigned char tempB;    // tempB[7,0] = ADC[7,0]
-unsigned char tempD;	// tempD[1,0] = ADC[9,8]
+unsigned short x ;      	// x holds the value of ADC
+unsigned char LED;      	// represents the state of B0
+unsigned short ADC_MAX = 0xFF ; // holds the highest ADC displayed 
 
+enum State {Start, ON_OFF} state; 
+
+void tick() {
+	// transitions 
+	switch(state) {
+		case Start: 
+			LED = 0x01;
+			state = ON_OFF;
+			break;
+
+		case ON_OFF:
+
+			LED = (x >= ADC_MAX/2) ? 0x01 : 0x00;
+			break;
+	}
+
+	// state actions
+	switch(state) {
+		case Start:
+			break;
+		case ON_OFF:
+			break;
+		default:
+			break;
+	}	
+} 
 
 void ADC_init() {
     ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1<< ADATE);
@@ -36,18 +61,16 @@ int main(void) {
     DDRA = 0x00; PORTA = 0xFF;  // input
     DDRB = 0xFF; PORTB = 0x00;  // output
     DDRD = 0xFF; PORTD = 0x00;  // output
-    
+  	
+    state = Start;  
     ADC_init();
 
     /* Insert your solution below */
     while (1) {
 	
-	x = ADC; 
-	tempB = (char) x;
-	tempD = (char) (x >> 8);
-        
-	PORTB = tempB; 
-	PORTD =	tempD;
+	x = ADC;
+	tick();	
+	PORTB = LED; 
     }
     return 1;
 }
